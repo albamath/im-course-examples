db7ad26a1c239687b16c3db1c1bfa1857a9d78f9
albamath <albam88@hotmail.com>
2026-02-13 11:36:42 +0100

    change api-three to a module

--- revolution/api-three.js
+++ revolution/api-three.js
@@ -1,217 +1,216 @@
 // initially copied from https://ysulyma.github.io/tools/revolution/api-three.js
-(() => {
-	const { cos, sin, PI } = Math,
-		TWOPI = 2 * PI;
 
-	const {
-		Color,
-		Curve,
-		CylinderGeometry,
-		DirectionalLight,
-		DoubleSide,
-		Group,
-		Matrix4,
-		Mesh,
-		MeshBasicMaterial,
-		MeshNormalMaterial,
-		MeshPhongMaterial,
-		OrbitControls,
-		ParametricGeometry,
-		PerspectiveCamera,
-		Scene,
-		SceneUtils,
-		TubeGeometry,
-		Vector3,
-		WebGLRenderer,
-	} = THREE;
+const { cos, sin, PI } = Math,
+	TWOPI = 2 * PI;
 
-	/* variables */
-	const settings = {
-		a: -2,
-		b: 2,
-		fn: (x) => Math.cos(x) + Math.sin(x) + 2,
-		slices: 10,
-		showGraph: true,
-	};
+const {
+	Color,
+	Curve,
+	CylinderGeometry,
+	DirectionalLight,
+	DoubleSide,
+	Group,
+	Matrix4,
+	Mesh,
+	MeshBasicMaterial,
+	MeshNormalMaterial,
+	MeshPhongMaterial,
+	OrbitControls,
+	ParametricGeometry,
+	PerspectiveCamera,
+	Scene,
+	SceneUtils,
+	TubeGeometry,
+	Vector3,
+	WebGLRenderer,
+} = THREE;
 
-	let camera, container, controls, renderer, scene;
-	let disks;
-	let graph;
+/* variables */
+const settings = {
+	a: -2,
+	b: 2,
+	fn: (x) => Math.cos(x) + Math.sin(x) + 2,
+	slices: 10,
+	showGraph: true,
+};
 
-	setup();
-	populateScene();
+let camera, container, controls, renderer, scene;
+let disks;
+let graph;
 
-	/* setup the scene */
-	function setup() {
-		scene = new THREE.Scene();
-		scene.background = new THREE.Color(0x333333);
-		container = document.getElementById("three-container");
+setup();
+populateScene();
 
-		setupRenderer();
-		setupCamera();
-		setupControls();
-		setupLights();
+/* setup the scene */
+function setup() {
+	scene = new THREE.Scene();
+	scene.background = new THREE.Color(0x333333);
+	container = document.getElementById("three-container");
 
-		container.appendChild(renderer.domElement);
+	setupRenderer();
+	setupCamera();
+	setupControls();
+	setupLights();
 
-		render();
+	container.appendChild(renderer.domElement);
 
-		function render() {
-			// render using requestAnimationFrame
-			requestAnimationFrame(render);
+	render();
 
-			controls.update();
+	function render() {
+		// render using requestAnimationFrame
+		requestAnimationFrame(render);
 
-			renderer.render(scene, camera);
-		}
+		controls.update();
 
-		window.addEventListener("resize", () => {
-			const { height, width } = container.getBoundingClientRect();
-
-			camera.aspect = width / height;
-			camera.updateProjectionMatrix();
-
-			renderer.setSize(width, height);
-		});
+		renderer.render(scene, camera);
 	}
 
-	function setupRenderer() {
-		renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
-		// renderer.shadowMapEnabled = true;
-
+	window.addEventListener("resize", () => {
 		const { height, width } = container.getBoundingClientRect();
 
+		camera.aspect = width / height;
+		camera.updateProjectionMatrix();
+
 		renderer.setSize(width, height);
+	});
+}
 
-		renderer.localClippingEnabled = true;
-	}
+function setupRenderer() {
+	renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+	// renderer.shadowMapEnabled = true;
 
-	function setupCamera() {
-		const { height, width } = renderer.getSize();
-		camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000);
+	const { height, width } = container.getBoundingClientRect();
 
-		// position and point the camera to the center of the scene
-		camera.up.set(0, 0, 1);
-		camera.position.x = 0;
-		camera.position.y = -5;
-		camera.position.z = 10;
-		camera.lookAt(new THREE.Vector3(0, -3, 5));
-	}
+	renderer.setSize(width, height);
 
-	function setupControls() {
-		// orbit controls
-		controls = new THREE.OrbitControls(camera, renderer.domElement);
-		controls.enableKeys = false;
-	}
+	renderer.localClippingEnabled = true;
+}
 
-	function setupLights() {
-		// lighting
-		const light = new THREE.AmbientLight(0x404040);
-		scene.add(light);
+function setupCamera() {
+	const { height, width } = renderer.getSize();
+	camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000);
 
-		const lights = [];
-		lights[0] = new THREE.PointLight(0xffffff, 1, 0);
-		lights[1] = new THREE.PointLight(0xffffff, 1, 0);
-		lights[2] = new THREE.PointLight(0xffffff, 1, 0);
+	// position and point the camera to the center of the scene
+	camera.up.set(0, 0, 1);
+	camera.position.x = 0;
+	camera.position.y = -5;
+	camera.position.z = 10;
+	camera.lookAt(new THREE.Vector3(0, -3, 5));
+}
 
-		lights[0].position.set(0, 200, 0);
-		lights[1].position.set(100, 200, 100);
-		lights[2].position.set(-100, -200, -100);
+function setupControls() {
+	// orbit controls
+	controls = new THREE.OrbitControls(camera, renderer.domElement);
+	controls.enableKeys = false;
+}
 
-		scene.add(lights[0]);
-		scene.add(lights[1]);
-		scene.add(lights[2]);
-	}
+function setupLights() {
+	// lighting
+	const light = new THREE.AmbientLight(0x404040);
+	scene.add(light);
 
-	function populateScene() {
-		scene.add(new THREE.AxisHelper(5));
+	const lights = [];
+	lights[0] = new THREE.PointLight(0xffffff, 1, 0);
+	lights[1] = new THREE.PointLight(0xffffff, 1, 0);
+	lights[2] = new THREE.PointLight(0xffffff, 1, 0);
 
-		var geometry = revolutionGeometry();
-		var material = new MeshPhongMaterial({
-			color: 0x00aeff,
+	lights[0].position.set(0, 200, 0);
+	lights[1].position.set(100, 200, 100);
+	lights[2].position.set(-100, -200, -100);
+
+	scene.add(lights[0]);
+	scene.add(lights[1]);
+	scene.add(lights[2]);
+}
+
+function populateScene() {
+	scene.add(new THREE.AxisHelper(5));
+
+	var geometry = revolutionGeometry();
+	var material = new MeshPhongMaterial({
+		color: 0x00aeff,
+	});
+	material.side = DoubleSide;
+	var mesh = new Mesh(geometry, material);
+	scene.add(mesh);
+	graph = mesh;
+
+	// disk/washer method
+	disks = new Group();
+	disks.visible = false; //!settings.showGraph;
+	scene.add(disks);
+}
+
+function revolutionGeometry(maxAngle = TWOPI) {
+	const surface = revolution(settings.fn, settings.a, settings.b, maxAngle);
+
+	return new ParametricGeometry(
+		(u, v) => new Vector3(...surface(u, v)),
+		120,
+		120,
+	);
+}
+
+function populateDisks() {
+	const { a, b, slices, fn } = settings;
+
+	const width = (b - a) / slices;
+
+	// clean up
+	while (disks.children.length) disks.remove(disks.children[0]);
+
+	// populate disks
+	for (let i = 0; i < slices; ++i) {
+		const p = a + i * width;
+
+		const geometry = new CylinderGeometry(fn(p), fn(p), width, 50);
+		const material = new MeshPhongMaterial({
+			color: 0x5cc26d,
 		});
-		material.side = DoubleSide;
-		var mesh = new Mesh(geometry, material);
-		scene.add(mesh);
-		graph = mesh;
+		const mesh = new Mesh(geometry, material);
 
-		// disk/washer method
-		disks = new Group();
-		disks.visible = false; //!settings.showGraph;
-		scene.add(disks);
+		mesh.position.set(p + width / 2, 0, 0);
+		mesh.rotation.z = Math.PI / 2;
+
+		disks.add(mesh);
 	}
+}
 
-	function revolutionGeometry(maxAngle = TWOPI) {
-		const surface = revolution(settings.fn, settings.a, settings.b, maxAngle);
+function revolution(f, a, b, maxAngle) {
+	return (s, t) => {
+		const x = a + s * (b - a),
+			angle = t * maxAngle;
 
-		return new ParametricGeometry(
-			(u, v) => new Vector3(...surface(u, v)),
-			120,
-			120,
-		);
-	}
-
-	function populateDisks() {
-		const { a, b, slices, fn } = settings;
-
-		const width = (b - a) / slices;
-
-		// clean up
-		while (disks.children.length) disks.remove(disks.children[0]);
-
-		// populate disks
-		for (let i = 0; i < slices; ++i) {
-			const p = a + i * width;
-
-			const geometry = new CylinderGeometry(fn(p), fn(p), width, 50);
-			const material = new MeshPhongMaterial({
-				color: 0x5cc26d,
-			});
-			const mesh = new Mesh(geometry, material);
-
-			mesh.position.set(p + width / 2, 0, 0);
-			mesh.rotation.z = Math.PI / 2;
-
-			disks.add(mesh);
-		}
-	}
-
-	function revolution(f, a, b, maxAngle) {
-		return (s, t) => {
-			const x = a + s * (b - a),
-				angle = t * maxAngle;
-
-			return [x, f(x) * cos(angle), f(x) * sin(angle)];
-		};
-	}
-
-	window.api3 = {
-		animate() {
-			const start = performance.now();
-
-			/** duration in milliseconds **/
-			duration = 2000;
-			function update(t) {
-				const progress = Math.min((t - start) / duration, 1);
-				console.log(start, t);
-				graph.geometry = revolutionGeometry(progress * TWOPI);
-				if (progress < 1) {
-					requestAnimationFrame(update);
-				}
-			}
-			requestAnimationFrame(update); // for the first call
-			graph.geometry = revolutionGeometry(PI);
-		},
-		update(o) {
-			Object.assign(settings, o);
-
-			if (o.a || o.b || o.fn) graph.geometry = revolutionGeometry();
-			graph.visible = settings.showGraph;
-
-			disks.visible = !settings.showGraph;
-			if (disks.visible) populateDisks();
-		},
+		return [x, f(x) * cos(angle), f(x) * sin(angle)];
 	};
+}
 
-	window.graph = graph;
-})();
+window.api3 = {
+	animate() {
+		const start = performance.now();
+
+		/** duration in milliseconds **/
+		duration = 2000;
+		function update(t) {
+			const progress = Math.min((t - start) / duration, 1);
+			console.log(start, t);
+			graph.geometry = revolutionGeometry(progress * TWOPI);
+			if (progress < 1) {
+				requestAnimationFrame(update);
+			}
+		}
+		requestAnimationFrame(update); // for the first call
+		graph.geometry = revolutionGeometry(PI);
+	},
+	update(o) {
+		Object.assign(settings, o);
+
+		if (o.a || o.b || o.fn) graph.geometry = revolutionGeometry();
+		graph.visible = settings.showGraph;
+
+		disks.visible = !settings.showGraph;
+		if (disks.visible) populateDisks();
+	},
+};
+
+window.graph = graph;
